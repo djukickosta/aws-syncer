7a244a0a2839fe643b40be2e352cc7d0875bac28
Kosta Djukic <djukickosta@gmail.com>
2019-08-09 01:32:03 +0100

    Implement CLI via click package, add option to list S3 bucket objects

--- aws-syncer.py
+++ aws-syncer.py
@@ -1,8 +1,26 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name='kosta')
 s3 = session.resource('s3')
 
-if __name__ == '__main__':
+@click.group()
+def cli():
+  """AWS-Syncer syncs directories to AWS."""
+  pass
+
+@cli.command('list-buckets')
+def list_buckets():
+  """List all S3 buckets."""
   for bucket in s3.buckets.all():
     print(bucket)
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+  """List objects in an S3 bucket."""
+  for obj in s3.Bucket(bucket).objects.all():
+    print(obj)
+
+if __name__ == '__main__':
+  cli()
